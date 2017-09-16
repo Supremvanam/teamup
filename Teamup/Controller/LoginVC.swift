d2e1b26ae90bd7a124ad1ffc9f3a28430ed05bff
Suprem Vanam <supremvanam.gsp@gmail.com>
2017-09-16 14:17:12 +0530

    Added Login and Registration

--- Teamup/Controller/LoginVC.swift
+++ Teamup/Controller/LoginVC.swift
@@ -10,26 +10,57 @@ import UIKit
 
 class LoginVC: UIViewController {
 
+    @IBOutlet weak var emailField: InsetTextField!
+    @IBOutlet weak var passwordField: InsetTextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        emailField.delegate = self
+        passwordField.delegate = self
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func signinBtnPressed(_ sender: Any) {
+        if emailField.text != nil && passwordField.text != nil {
+            AuthService.instance.loginUser(withEmail: emailField.text!, andPassword: passwordField.text!, loginComplete: { (success, loginError) in
+                if success {
+                    self.dismiss(animated: true, completion: nil)
+                    print("Successfully logged in")
+                } else {
+                    print(String(describing: loginError?.localizedDescription))
+                }
+                
+                AuthService.instance.registerUser(withEmail: self.emailField.text!, andPassword: self.passwordField.text!, userCreationComplete: { (success, registrationError) in
+                    if success {
+                        print("Successfully registered user")
+                        AuthService.instance.loginUser(withEmail: self.emailField.text!, andPassword: self.passwordField.text!, loginComplete: { (success, loginError) in
+                            if success {
+                                self.dismiss(animated: true, completion: nil)
+                                print("Successfully logged in")
+                            } else {
+                                print(String(describing : loginError?.localizedDescription))
+                            }
+                        })
+                        
+                        
+                    } else {
+                        print(String(describing: registrationError?.localizedDescription))
+                    }
+                    
+                })
+                
+            })
+        }
+    
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    
+    @IBAction func closeBtnPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
     }
-    */
+}
 
+extension LoginVC: UITextFieldDelegate {
+    
 }
