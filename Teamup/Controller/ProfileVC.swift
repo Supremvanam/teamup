caa36885901db3239e8c44b2eb8d601067c41622
Suprem Vanam <supremvanam.gsp@gmail.com>
2017-09-19 17:22:03 +0530

    Added ProfileVC and CreatePostVC

---
+++ Teamup/Controller/ProfileVC.swift
@@ -0,0 +1,29 @@
+//
+//  ProfileVC.swift
+//  Teamup
+//
+//  Created by Suprem Vanam on 19/09/17.
+//  Copyright © 2017 Suprem Vanam. All rights reserved.
+//
+
+import UIKit
+
+class ProfileVC: UIViewController {
+
+    @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var emailLabel: UILabel!
+    @IBOutlet weak var tableView: UITableView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+    @IBAction func logoutBtnPressed(_ sender: Any) {
+    
+    
+    }
+    
+}
